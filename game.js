a45b1fb4cf46a34983daf4008d2fc4b377fc48f4
skeown13 <code@keown.net>
2020-06-12 16:50:36 -0700

    added functions for feed, sleep, play, and increaseAge to the DigitalPet constructor

--- game.js
+++ game.js
@@ -4,5 +4,36 @@ function DigitalPal () {
   this.bored = true
   this.age = 0
 
-  
+  this.feed = function() {
+    if (this.hungry) {
+      console.log("That was yummy!")
+      this.hungry = false
+      this.sleepy = true
+    } else {
+      console.log("No thanks! I'm full.")
+    }
+  }
+  this.sleep = function() {
+    if (this.sleepy) {
+      console.log("Zzzzzzzz")
+      this.sleepy = false
+      this.bored = true
+      this.increaseAge()
+    } else {
+      "No way! I'm not tired."
+    }
+  }
+  this.play = function() {
+    if (this.bored) {
+      console.log("Yay! Let's play!")
+      this.bored = false
+      this.hungry = true
+    } else {
+      console.log("Not right now. Later?")
+    }
+  }
+  this.increaseAge = function() {
+    this.age += 1
+    console.log("Happy Birthday to me! I am " + this.age + " old!")
+  }
 }
