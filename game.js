1a77e85b5b9458b0277ce03f824bef4b50029fb4
skeown13 <code@keown.net>
2020-06-29 11:18:30 -0700

    created file elephant.js and moved all elephant functionality from game.js to elephant.js.  Read elephant.js in game.js with fs.readFileSync

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ eval(fs.readFileSync("dog.js") + "")
 eval(fs.readFileSync("cat.js") + "")
 eval(fs.readFileSync("owl.js") + "")
 eval(fs.readFileSync("bunny.js") + "")
+eval(fs.readFileSync("elephant.js") + "")
 
 // The Original Constructor used as the base for all animals
 
@@ -170,110 +171,3 @@ inquirer.prompt([
 
   }
 })
-
-// Starts the Functionality for ELEPHANT
-function elephantInteract() {
-  inquirer.prompt([
-    {
-      type: "list",
-      name: "elephantOpt",
-      message: "What would you like to do?",
-      choices: ["Play", "Eat", "Sleep"]
-    }
-  ]).then(function(answers) {
-    if (answers.elephantOpt === "Play") {
-      elephantPlay()
-    } else if (answers.elephantOpt === "Eat") {
-      elephantEat()
-    } else if (answers.elephantOpt === "Sleep") {
-      elephantSleep()
-    }
-  })
-}
-
-function elephantPlay() {
-  if (elephant.bored) {
-    elephant.hungry = true
-  } else {
-    console.log("no")
-  }
-
-  elephant.play(elephantPlayArray)
-
-  elephantInteract()
-}
-
-let elephantPlayArray = [elephantTrain, elephantTrumpet, elephantCircus]
-
-function elephantTrain() {
-  console.log("")
-  console.log("                       _       __,\"\\\\  _.")
-  console.log("    /\\\\     /\\\\   _.--/ `.   ,' (   \"_//\"")
-  console.log(" ,''\\,\"  ,\"\"\\,\"=='(   \\.'\\\\_,(  _`-'=-'")
-  console.log("' u-u `=' u-u     |l''|l  `\" |m' |m")
-  console.log("Choo Choo!! Here comes the Elephant Train! Choo Choo!!")
-  console.log("")
-}
-
-function elephantTrumpet() {
-  console.log("")
-  console.log("        .--._   _,-';")
-  console.log("   ,---(    .`-',-'")
-  console.log(" /(     `--'_,'")
-  console.log("(  \\ |__| |'")
-  console.log("   |_|  |_|")
-  console.log("Toot Toot!! My trunk makes a noise like a trumpet! Toot Toot!!")
-  console.log("")
-}
-
-function elephantCircus() {
-  console.log("")
-  console.log("           .-~~-. .--.")
-  console.log("          /      /   \"\\")
-  console.log("         /    \\ |  \\   \\")
-  console.log("        |/\\    | \\_/-'\\ \\")
-  console.log("        A |   /  ||   /_/")
-  console.log("          |__||__||")
-  console.log("         /~~~~~~~~~~\\")
-  console.log("        /\\_/\\_/\\_/\\_/\\")
-  console.log("       /\\/ \\/ \\/ \\/ \\/\\")
-  console.log("    __/________________\\__")
-  console.log("Look at me! Look at me!! I'm like one of those Circus Elephants!")
-  console.log("")
-}
-
-function elephantEat() {
-  if (elephant.hungry) {
-    console.log("")
-    console.log(" ,+.")
-    console.log("((|))        ,+.")
-    console.log(" )|(   ,+.  ((|))")
-    console.log("((|)) ((|))  )|(")
-    console.log(" `-'   )|(  ((|))")
-    console.log("      ((|))  `-'")
-    console.log("       `-'  ")
-    console.log("")
-  } else {
-    console.log("We can't eat yet... We haven't even played!")
-  }
-
-  elephant.eat()
-
-  elephantInteract()
-}
-
-function elephantSleep() {
-  if (elephant.sleepy) {
-    console.log("")
-    console.log(" z      ____")
-    console.log("    z  /_   \\__")
-    console.log("  z   /_  \\_/  \\")
-    console.log("   z // \\  ___ |\\")
-    console.log("         |_| |_|")
-    console.log("")
-  }
-
-  elephant.sleep()
-
-  elephantInteract()
-}
