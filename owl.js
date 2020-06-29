219222e7f2f24a0ac8a200026a7a8a1b79d3e0af
skeown13 <code@keown.net>
2020-06-29 11:13:41 -0700

    created file owl.js and moved all owl functionality from game.js to owl.js.  Read owl.js in game.js with fs.readFileSync

---
+++ owl.js
@@ -0,0 +1,78 @@
+// Starts the Functionality for OWL
+function owlInteract() {
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "owlOpt",
+      message: "What would you like to do?",
+      choices: ["Fly", "Hunt", "Eat", "Sleep"]
+    }
+  ]).then(function(answers) {
+    if (answers.owlOpt === "Fly") {
+      owlFly()
+    } else if (answers.owlOpt === "Eat") {
+      owlEat()
+    } else if (answers.owlOpt === "Sleep") {
+      owlSleep()
+    }
+  })
+}
+
+function owlFly() {
+  console.log("")
+  console.log("__________-------____                 ____-------__________")
+  console.log("\\------____-------___--__---------__--___-------____------/")
+  console.log(" \\//////// / / / / / \\   _-------_   / \\ \\ \\ \\ \\ \\\\\\\\\\\\\\\\/")
+  console.log("   \\////-/-/------/_/_| /___   ___\\ |_\\_\\------\\-\\-\\\\\\\\/")
+  console.log("     --//// / /  /  //|| (O)\\ /(O) ||\\\\  \\  \\ \\ \\\\\\\\--")
+  console.log("          ---__/  // /| \\_  /V\\  _/ |\\ \\\\  \\__---")
+  console.log("               -//  / /\\_ ------- _/\\ \\  \\\\-")
+  console.log("                 \\_/_/ /\\---------/\\ \\_\\_/")
+  console.log("                     ----\\   |   /----")
+  console.log("                          | -|- |")
+  console.log("                         /   |   \\")
+  console.log("                         ---- \\___|")
+
+  owl.fly()
+
+  owlInteract()
+}
+
+function owlEat() {
+  if (owl.hungry) {
+    console.log("")
+    console.log("()(),~~,.")
+    console.log(" xx ___; )")
+    console.log("=`=     (_.")
+    console.log("")
+  } else {
+    console.log("There's no such thing as a free meal! We must hunt our food if we want to eat...")
+  }
+
+  owlInteract()
+}
+
+function owlSleep() {
+  if (owl.sleepy) {
+    console.log("")
+    console.log("   ...    *    .   _  .")
+    console.log("*  .  *     .   * (_)   *")
+    console.log("  .      |*  ..   *   ..")
+    console.log("   .  * \\|  *  ___  . . *")
+    console.log("*   \\/   |/ \\/{o,o}     .")
+    console.log("  _\\_\\   |  / /)  )* _/_ *")
+    console.log("      \\ \\| /,--\"-\"---  ..")
+    console.log("_-----`  |(,__,__/__/_ .")
+    console.log("       \\ ||      ..")
+    console.log("        ||| .            *")
+    console.log("        |||")
+    console.log("        |||")
+    console.log("  , -=-~' .-^- _")
+    console.log("           `")
+    console.log("")
+  } 
+
+  owl.sleep()
+
+  owlInteract()
+}
